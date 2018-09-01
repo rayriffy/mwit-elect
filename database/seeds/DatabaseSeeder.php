0c628ab527d8a1505b1d38be59d121af3832790f
Phumrapee Limpianchop <git@rayriffy.com>
2018-09-01 13:17:08 +0700

    :card_file_box: Database Seeder to seed sample elections

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,5 +23,39 @@ class DatabaseSeeder extends Seeder
         $user->is_admin = false;
         $user->expire = Carbon::now()->addYears(1);
         $user->save();
+
+        $test[0] = str_random(64);
+        $election = new App\ELECTION;
+        $election->election_id = $test[0];
+        $election->election_name = "Test 1";
+        $election->election_start = Carbon::now()->addHours(2);
+        $election->election_end = Carbon::now()->addDay();
+        $election->save();
+
+        $test[1] = str_random(64);
+        $election = new App\ELECTION;
+        $election->election_id = $test[1];
+        $election->election_name = "Test 2";
+        $election->election_start = Carbon::now()->addDays(2);
+        $election->election_end = Carbon::now()->addDay(5);
+        $election->save();
+
+        $test[2] = str_random(64);
+        $election = new App\ELECTION;
+        $election->election_id = $test[2];
+        $election->election_name = "Test 3";
+        $election->election_start = Carbon::now()->subHours(2);
+        $election->election_end = Carbon::now()->addDay(1);
+        $election->save();
+
+        for($i = 0 ; $i < 3 ; $i++) {
+            for($j = 0 ; $j < 5 ; $j++) {
+                $candidate = new App\CANDIDATE;
+                $candidate->candidate_id = str_random(128);
+                $candidate->election_id = $test[$i];
+                $candidate->candidate_name = "Guy " . $j;
+                $candidate->save();
+            }
+        }
     }
 }
